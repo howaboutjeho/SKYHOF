305c9f71f69f87712e144f130c25643524961b66
Jehovany Cruz <jehovany@Jehovanys-MacBook-Air.local>
2020-06-23 12:34:42 -0400

    node_express_bodyparser_setup

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const bodyParser = require('body-parser');
+
+const app = express();
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+//
+app.post('/modify', (req, res) => {
+    console.log(req.body);
+  });
+
+app.listen(3000, function () {
+  console.log("listening on 3000");
+});
